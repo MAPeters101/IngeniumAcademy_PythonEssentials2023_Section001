826f587d6924d61767a101269d81abc9e6d1c178
Mark Peters <mapeters101@earthlink.net>
2025-02-12 00:29:38 -0500

    Work on lesson001.

---
+++ lesson001.py
@@ -0,0 +1,14 @@
+"""
+Numbers: eg. int, float, complex
+Text Type: str
+Boolean Type: bool
+"""
+
+if __name__ == '__main__':
+    print("Bool:", True)
+    print("String:", "Hello, World!")
+    print("Float:", 10.0)
+    print("Integer:", 10)
+
+    print("The type of False", type(False))
+    print("The type of Hello, World!", type("Hello, World!"))
