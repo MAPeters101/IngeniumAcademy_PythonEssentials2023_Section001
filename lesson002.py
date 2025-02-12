75497575652273b51e8e6c4f540fa1f64270152e
Mark Peters <mapeters101@earthlink.net>
2025-02-12 00:46:40 -0500

    Work on lesson002.

---
+++ lesson002.py
@@ -0,0 +1,16 @@
+"""
+Variables are used to store data values
+"""
+
+if __name__ == '__main__':
+    age = 55
+    name = "Jane Doe"
+    job = 'Software Developer'
+
+    print("Age:", age)
+    print("Name:", name)
+    print("Job:", job)
+
+    age = 36
+    print("Age:", age)
+
