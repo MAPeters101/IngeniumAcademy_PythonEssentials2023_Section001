5948d7be4697ab14bf9f7294f93c9e08a89f52c1
Mark Peters <mapeters101@earthlink.net>
2025-02-13 21:05:08 -0500

    Complete work on lesson004 from 1:50.

--- lesson004.py
+++ lesson004.py
@@ -2,3 +2,11 @@ if __name__ == '__main__':
     print(17 == 17)
     print(23 == 17)
     print(18 > 17)
+
+    print("16" < "17")
+    print("100" < "11")
+    # Reason
+    print(ord("1"), ord("0"), ord("0"))
+    print(ord("1"), ord("1"))
+
+    print((True and False) == False)
