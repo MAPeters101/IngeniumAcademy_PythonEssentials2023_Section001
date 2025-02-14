74f4318363714d231ed6d4ae1cdc4a82fa7da280
Mark Peters <mapeters101@earthlink.net>
2025-02-13 21:09:11 -0500

    Work on lesson005 to 0:50.

---
+++ lesson005.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    x = 10
+    y = 3
+
+    print('Addition:', x + y)
+    print('Subtraction:', x - y)
+    print('Multiplication:', x * y)
+    print('Division:', x / y)
