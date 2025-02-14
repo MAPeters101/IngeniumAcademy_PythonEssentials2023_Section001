6701704faf9b703676aaaab74eed043b8b6868c2
Mark Peters <mapeters101@earthlink.net>
2025-02-13 20:57:14 -0500

    Work on lesson003 to 4:30.

---
+++ lesson003.py
@@ -0,0 +1,13 @@
+
+if __name__ == '__main__':
+    string = "Python"
+    print(string[0])
+
+    # Slicing
+    print(string[1:4])
+    print(string[::2])
+
+    # Immutability
+    #string[0] = "C"
+
+    
