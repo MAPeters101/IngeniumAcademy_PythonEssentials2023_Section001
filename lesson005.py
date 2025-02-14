c17be75798a979904cbb5636737ea1551755f732
Mark Peters <mapeters101@earthlink.net>
2025-02-13 21:49:22 -0500

    Complete work on lesson005 from 0:50.

--- lesson005.py
+++ lesson005.py
@@ -6,3 +6,20 @@ if __name__ == '__main__':
     print('Subtraction:', x - y)
     print('Multiplication:', x * y)
     print('Division:', x / y)
+
+    print('Modulus:', x % y)
+
+    print('*'*25)
+
+    x = 3
+    y = 10
+
+    print('Modulus:', x % y)
+    print('Exponentiation:', x ** y)
+
+    x = 10
+    y = 3
+
+    print('Floor Division:', x // y)
+
+
